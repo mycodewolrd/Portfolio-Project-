d14189241d6fde6434419c6ac7412802ab80ba31
Rajnandini <rajnandinipatra2003@gmail.com>
2025-03-24 15:10:57 +0530

    App.jsx and index.css file updated for Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Navbar from "./components/NavBar/navbar.jsx";
 import MobileMenu from "./components/NavBar/MobileMenu.jsx";
+import Hero from "./components/NavBar/Hero.jsx";
 
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
       <div>
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <Hero />
       </div>
     );
 };
